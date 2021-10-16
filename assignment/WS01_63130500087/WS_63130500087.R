3147821361ec9d23621ba7236c1aff448c292604
Cartoonydesu <toon.pi45@gmail.com>
2021-10-16 11:20:13 +0700

    add details

--- assignment/WS01_63130500087/WS_63130500087.R
+++ assignment/WS01_63130500087/WS_63130500087.R
@@ -1,7 +1,34 @@
 # 63130500087
-# Step 0
+
+# Step 0-------------------
 library(dplyr)
+sat_score <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/SAT_original.csv")
+count(sat_score) #485 rows
 
-# Explore
+# Explore data in dataset (Diagnosis)---------------
+glimpse(sat_score)
+View(sat_score)
 
-# xxxx
+# Checking the type of value-----------------------
+#DBN
+is.character(sat_score$DBN)
+#school_name
+is.character(sat_score$school_name)
+#math_score
+is.numeric(sat_score$math_score)
+#reading_score
+is.numeric(sat_score$reading_score)
+#writing_score
+is.numeric(sat_score$writing_score)
+
+
+#check is null?
+is.na(sat_score$DBN)
+assert_all_are_na(sat_score$DBN)
+sat_score %>% filter(is.na(DBN)|
+                       is.na(school_name)|
+                       is.na(math_score)|
+                       is.na(reading_score)|
+                       is.na(writing_score))
+#there are no NA value
+
